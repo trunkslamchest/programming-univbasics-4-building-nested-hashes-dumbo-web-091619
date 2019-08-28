04cd9f3473f91f031270e9fd6f90d0fd6514b3e8
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 23:21:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,8 +41,14 @@ def monopoly_with_third_tier
             {
               "mortgage_value" => "$100"
             },
-          :pennsylvania_railroad => {},
-          :b_and_o_railroad => {},
+          :pennsylvania_railroad => 
+            {
+              "mortgage_value" => "$200"
+            },
+          :b_and_o_railroad =>
+            {
+              "mortgage_value" => "$400"
+            },
           :shortline_railroad => {}
         }
   	  }
