8f0377b60fd13390f3fcca525518366b318c993f
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 23:16:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,9 @@ def monopoly_with_third_tier
   	      :rent_in_dollars =>
   	        {
   	          :one_piece_owned => 25,
-  	          
+  	          :two_pieces_owned => 50,
+  	          :three_pieces_owned => 75,
+  	          :four_pieces_owned => 100
   	        }
   	  }
 	}
